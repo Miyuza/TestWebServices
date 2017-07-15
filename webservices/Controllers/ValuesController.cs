b8a806fd84845d12f90b8c0f72c418c7addcf4e4
Miyuza <sipteu@gmail.com>
2017-07-15 13:57:13 +0500

    1st

---
+++ webservices/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webservices.Models;
+
+namespace webservices.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<person> Get()
+        {
+            return DBHelper.DAL.get();
+        }
+
+        // GET api/values/5
+        public person Get(int id)
+        {
+            return DBHelper.DAL.get(id);
+        }
+
+        // POST api/values
+        public void Post([FromBody]person p)
+        {
+            DBHelper.DAL.post(p.id,p.name);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]person p)
+        {
+            DBHelper.DAL.put(id, p.name);
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ webservices/DBHelper/DAL.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using webservices.Models;
+
+namespace webservices.DBHelper
+{
+    public class DAL
+    {
+        public static List<person> get()
+        {
+            String q = "";
+            List<person> lst = new List<person>();
+            person p = null;
+            q = String.Format("selest * from Test");
+            using (DBHelper db = new DBHelper())
+            {
+                SqlDataReader dr =  db.ExecuteReader(q);
+                while (dr.Read())
+                {
+                    p = new person();
+                    p.id = dr.GetInt32(0);
+                    p.name = dr.GetString(1);
+                    lst.Add(p);
+                }
+            }
+            return lst;
+        }
+        public static person get(int id)
+        {
+            String q = "";
+            List<person> lst = new List<person>();
+            person p = null;
+            q = String.Format("selest * from Test where id = "+id);
+            using (DBHelper db = new DBHelper())
+            {
+                SqlDataReader dr = db.ExecuteReader(q);
+                if (dr.Read())
+                {
+                    p = new person();
+                    p.id = dr.GetInt32(0);
+                    p.name = dr.GetString(1);
+                    
+                }
+            }
+            return p;
+        }
+        public static void post(int id,String name)
+        {
+            String q = "";
+            q = String.Format("insert into Test(id,name) values("+id+",'"+name+"')");
+            using (DBHelper db = new DBHelper())
+            {
+                db.ExecuteQuery(q);
+            }
+        }
+        public static void put(int id, String name)
+        {
+            String q = "";
+            q = String.Format("update Test set name = '"+name+"' where id = "+id);
+            using (DBHelper db = new DBHelper())
+            {
+                db.ExecuteQuery(q);
+            }
+        }
+    }
+}
